c5546f6a8196b3892262e94d6ae77d9c84126e36
Sergio Oliveira <sergio@tracy.com.br>
2013-08-15 13:06:07 -0300

    Making sure proxing works even with csrf active

--- revproxy/views.py
+++ revproxy/views.py
@@ -2,10 +2,12 @@
 import urllib2
 from urlparse import urljoin
 
+from django.views.decorators.csrf import csrf_exempt
+
 from .response import HttpProxyResponse
 from .utils import normalize_headers
 
- 
+@csrf_exempt
 def proxy(request, path, base_url):
 
     request_headers = normalize_headers(request)
@@ -13,7 +15,7 @@ def proxy(request, path, base_url):
     if request.user and request.user.is_active:
         request_headers['REMOTE_USER'] = request.user
 
-    request_url = urljoin(base_url, path) 
+    request_url = urljoin(base_url, path)
     proxy_request = urllib2.Request(request_url, headers=request_headers)
 
     try:
