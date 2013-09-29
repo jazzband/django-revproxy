99a8f6b900069ce4fbb710568329841c3cb66703
Sergio Oliveira <sergio@tracy.com.br>
2013-09-28 21:27:51 -0300

    Using CBV for proxyview

--- revproxy/views.py
+++ revproxy/views.py
@@ -5,39 +5,41 @@ from urlparse import urljoin
 
 from django.conf import settings
 from django.views.decorators.csrf import csrf_exempt
+from django.views.generic import View
 
 from .response import HttpProxyResponse
 from .utils import normalize_headers, NoHTTPRedirectHandler
 
 
-@csrf_exempt
-def proxy(request, path, base_url):
+class ProxyView(View):
+    base_url = None
+    add_remote_user = False
+    diazo_rules = None
 
-    add_remote_user = getattr(settings, 'REVPROXY_ADD_REMOTE_USER', False)
+    @csrf_exempt
+    def dispatch(self, request, path):
+        request_headers = normalize_headers(request)
 
-    request_headers = normalize_headers(request)
+        if self.add_remote_user and request.user.is_active:
+            request_headers['REMOTE_USER'] = request.user.username
 
-    if add_remote_user and request.user and request.user.is_active:
-        request_headers['REMOTE_USER'] = request.user.username
+        request_url = urljoin(self.base_url, path)
 
-    request_url = urljoin(base_url, path)
+        if request.GET:
+            request_url += '?' + urllib.urlencode(request.GET.items())
 
-    if request.GET:
-        request_url += '?' + urllib.urlencode(request.GET.items())
+        proxy_request = urllib2.Request(request_url, headers=request_headers)
 
-    proxy_request = urllib2.Request(request_url, headers=request_headers)
+        if request.POST:
+            post_data = request.POST.items()
+            proxy_request.add_data(urllib.urlencode(post_data))
 
-    if request.POST:
-        post_data = request.POST.items()
-        proxy_request.add_data(urllib.urlencode(post_data))
+        opener = urllib2.build_opener(NoHTTPRedirectHandler)
+        urllib2.install_opener(opener)
 
-    opener = urllib2.build_opener(NoHTTPRedirectHandler)
-    urllib2.install_opener(opener)
+        try:
+            proxy_response = urllib2.urlopen(proxy_request)
+        except urllib2.HTTPError as e:
+            proxy_response = e
 
-    try:
-        proxy_response = urllib2.urlopen(proxy_request)
-    except urllib2.HTTPError as e:
-        proxy_response = e
-
-    return HttpProxyResponse(proxy_response)
-
+        return HttpProxyResponse(proxy_response)
