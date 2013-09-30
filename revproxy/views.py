cfb713b132eb680cc7f73437bfcc9b43cfa12b5d
Sergio Oliveira <sergio@tracy.com.br>
2013-09-30 16:49:10 -0300

    Adding diazo middleware to process templates

--- revproxy/views.py
+++ revproxy/views.py
@@ -15,6 +15,7 @@ class ProxyView(View):
     base_url = None
     add_remote_user = False
     diazo_rules = None
+    diazo_theme_template = None
 
     @csrf_exempt
     def dispatch(self, request, path):
@@ -41,5 +42,8 @@ class ProxyView(View):
             proxy_response = urllib2.urlopen(proxy_request)
         except urllib2.HTTPError as e:
             proxy_response = e
+
+        request.diazo_rules = self.diazo_rules
+        request.diazo_theme_template = self.diazo_theme_template
 
         return HttpProxyResponse(proxy_response)
