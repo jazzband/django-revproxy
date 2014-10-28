1b94c22fc53aa0984946436a266e14677da01280
Sergio Oliveira <sergio@tracy.com.br>
2014-10-28 15:49:36 -0200

    Added travis and tox files

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     version='0.2.8',
     long_description=read('README.rst'),
     packages=['revproxy'],
-    test_suite = "tests.run.runtests",
+    test_suite="tests.run.runtests",
     author='Sergio Oliveira',
     author_email='sergio@tracy.com.br',
     url='https://github.com/TracyWebTech/django-revproxy',
