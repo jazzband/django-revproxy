91a5b092ec4a9df37fb5355e01cbce490f2b483a
Alexandre Barbosa <alexandreab@live.com>
2014-11-03 15:45:47 -0200

    Improving HttpProxyResponse tests
    Signed-off-by: Alexandre Barbosa <alexandreab@live.com>
    Signed-off-by: Carlos Oliveira <carlospecter@gmail.com>

--- tests/test_response.py
+++ tests/test_response.py
@@ -1,23 +1,45 @@
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
 from revproxy.response import HttpProxyResponse
 
+from mock import patch
+
 from urllib2 import urlopen, Request
+
+from revproxy.views import ProxyView
+from revproxy.utils import DEFAULT_CHARSET
 
 
 class ResponseTest(TestCase):
     def setUp(self):
-        proxy_request = Request('http://www.example.com')
-        self.proxy_response = urlopen(proxy_request)
+        self.factory = RequestFactory()
+
+        self.urllib2_urlopen_patcher = patch('revproxy.views.urlopen')
+        self.response_patcher = patch('revproxy.views.HttpProxyResponse')
+
+        self.urllib2_urlopen = self.urllib2_urlopen_patcher.start()
+        self.responser = self.response_patcher.start()
+
+
+    def tearDown(self):
+        self.urllib2_urlopen_patcher.stop()
+        self.responser = self.response_patcher.stop()
 
     def test_unicode_content_is_not_none(self):
-        self.proxy_response.headers['Content-Type'] = "text/html; charset=UTF-8"
-        response = HttpProxyResponse(self.proxy_response)
+        class CustomProxyView(ProxyView):
+            upstream = 'http://www.example.com'
+
+        request = self.factory.get('/')
+        response = CustomProxyView.as_view()(request, '/')
 
         self.assertIsNotNone(response.unicode_content)
 
-    def test_unicode_content_is_none(self):
-        self.proxy_response.headers['Content-Type'] = "text/html;"
-        response = HttpProxyResponse(self.proxy_response)
+    def test_charset_is_default_charset(self):
+        class CustomProxyView(ProxyView):
+            upstream = 'http://www.example.com'
 
-        self.assertIsNone(response.unicode_content)
+        utf8_content_type = 'text/html'
 
+        response = self.factory.get('/',CONTENT_TYPE=utf8_content_type)
+        http_response = CustomProxyView.as_view()(response, '/')
+    
+         
