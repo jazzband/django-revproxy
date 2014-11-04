d4080bb78c684d9a1d41eed42a094e9379d91a09
Carlos Oliveira <carlospecter@gmail.com>
2014-11-04 20:29:55 -0200

    Tests for charset in Content-Type
    Added response factory as a parameter to patcher so that it returns
    proxy response instead of a magic mock

--- tests/utils.py
+++ tests/utils.py
@@ -1,21 +1,18 @@
+import sys
 
 if sys.version_info >= (3, 0, 0):
     from urllib.response import addinfourl
 else:
     from urllib import addinfourl
 
-from io import StringIO
+from io import BytesIO
 
 
 def response_like_factory(proxy_request, headers, retcode):
     def get_response_like_object(proxy_request):
-        fp_like = StringIO('Fake file')
+        fp_like = BytesIO('Fake file')
 
         url = proxy_request.get_full_url()
         return addinfourl(fp_like, headers, url, retcode)
 
     return get_response_like_object
-
-
-#proxy_response = response_like_factory(proxy_request, headers, retcode)
-#patch('revproxy.views.urlopen', new=proxy_response)
