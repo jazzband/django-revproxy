f6d40c0cbc4a983aa59d4671154373525004ef67
Sergio Oliveira <sergio@tracy.com.br>
2014-11-24 16:55:01 -0200

    Removed charset test. Nothing really tested here.

--- tests/test_response.py
+++ tests/test_response.py
@@ -1,7 +1,6 @@
 from django.test import RequestFactory, TestCase
 from mock import patch
 from revproxy.response import HOP_BY_HOP_HEADERS
-from revproxy.utils import DEFAULT_CHARSET, get_charset
 from revproxy.views import ProxyView
 
 from .utils import response_like_factory
@@ -19,22 +18,6 @@ class ResponseTest(TestCase):
     def tearDown(self):
         CustomProxyView.upstream = "http://www.example.com"
         CustomProxyView.diazo_rules = None
-
-    def test_charset_is_not_default_charset(self):
-        path = "/"
-        headers = {'Content-Type': "text/html; charset=utf-8"}
-        request = self.factory.get(path)
-
-        get_proxy_response = response_like_factory(request, headers, 200)
-
-        urllib2_urlopen_patcher = patch(
-            'revproxy.views.urlopen',
-            new=get_proxy_response
-        )
-        with urllib2_urlopen_patcher:
-            response = CustomProxyView.as_view()(request, path)
-            charset = get_charset(response['Content-Type'])
-            self.assertNotEqual(DEFAULT_CHARSET, charset)
 
     def test_location_replaces_request_host(self):
         headers = {'Location': 'http://www.example.com'}
