8c23fe356f0f26fd2d46db155f092f9c45cc545c
Macartur Sousa <macartur.sc@gmail.com>
2015-02-26 10:30:30 -0300

    Added comments to utils.py
    Signed-off-by: Macartur Sousa <macartur.sc@gmail.com>
    Signed-off-by: Lucas Moura <lucas.moura128@gmail.com>

--- revproxy/utils.py
+++ revproxy/utils.py
@@ -3,31 +3,42 @@ import re
 
 import logging
 
-
+#: List containing string constant that are used to represent headers that can
+#: be ignored in the required_header function
 IGNORE_HEADERS = (
     'HTTP_ACCEPT_ENCODING',  # We want content to be uncompressed so
-                             #   we remove the Accept-Encoding from
-                             #   original request
+                             # we remove the Accept-Encoding from
+                             # original request
     'HTTP_HOST',
 )
 
 
 # Default from HTTP RFC 2616
 #   See: http://www.w3.org/Protocols/rfc2616/rfc2616-sec3.html#sec3.7.1
+#: Variable that represent the default charset used
 DEFAULT_CHARSET = 'latin-1'
 
+#: List containing string constants that represents possible html content type
 HTML_CONTENT_TYPES = (
     'text/html',
     'application/xhtml+xml'
 )
 
+#: Variable used to represent a minimal content size required for response
+#: to be turned into stream
 MIN_STREAMING_LENGTH = 4 * 1024  # 4KB
 
-
+#: Regex used to find charset in a html content type
 _get_charset_re = re.compile(r';\s*charset=(?P<charset>[^\s;]+)', re.I)
 
 
 def is_html_content_type(content_type):
+    """Function used to verify if the parameter is a proper html content type
+
+    :param content_type: String variable that represent a content-type
+    :returns:  A boolean value stating if the content_type is a valid html
+               content type
+    """
     for html_content_type in HTML_CONTENT_TYPES:
         if content_type.startswith(html_content_type):
             return True
@@ -36,6 +47,15 @@ def is_html_content_type(content_type):
 
 
 def should_stream(proxy_response):
+    """Function to verify if the proxy_response must be converted into
+    a stream.This will be done by checking the proxy_response content-length
+    and verify if its length is bigger than one stipulated
+    by MIN_STREAMING_LENGTH.
+
+    :param proxy_response: An Instance of urllib3.response.HTTPResponse
+    :returns: A boolean stating if the proxy_response should
+              be treated as a stream
+    """
     content_type = proxy_response.headers.get('Content-Type')
 
     if is_html_content_type(content_type):
@@ -53,6 +73,13 @@ def should_stream(proxy_response):
 
 
 def get_charset(content_type):
+    """Function used to retrieve the charset from a content-type.If there is no
+    charset in the content type then the charset defined on DEFAULT_CHARSET
+    will be returned
+
+    :param  content_type:   A string containing a Content-Type header
+    :returns:               A string containing the charset
+    """
     if not content_type:
         return DEFAULT_CHARSET
 
@@ -64,6 +91,11 @@ def get_charset(content_type):
 
 
 def required_header(header):
+    """Function that verify if the header parameter is a essential header
+
+    :param header:  A string represented a header
+    :returns:       A boolean value that represent if the header is required
+    """
     if header in IGNORE_HEADERS:
         return False
 
@@ -74,6 +106,12 @@ def required_header(header):
 
 
 def normalize_headers(request):
+    """Function used to transform header, replacing 'HTTP\_' to ''
+    and replace '_' to '-'
+
+    :param request:  A HttpRequest that will be transformed
+    :returns:        A dictionary with the normalized headers
+    """
     norm_headers = {}
     for header, value in request.META.items():
         if required_header(header):
@@ -84,6 +122,12 @@ def normalize_headers(request):
 
 
 def encode_items(items):
+    """Function that encode all elements in the list of items passed as
+    a parameter
+
+    :param items:  A list of tuple
+    :returns:      A list of tuple with all items encoded in 'utf-8'
+    """
     encoded = []
     for key, values in items:
         for value in values:
@@ -100,6 +144,8 @@ def cookie_from_string(cookie_string):
     django's response.set_cookie method. Because set_cookie doesn't
     have parameter comment, this cookie attribute will be ignored.
 
+    :param  cookie_string: A string representing a valid cookie
+    :returns: A dictionary containing the cookie_string attributes
     """
 
     valid_attrs = ('path', 'domain', 'comment', 'expires',
@@ -136,7 +182,7 @@ def cookie_from_string(cookie_string):
                 cookie_dict[attr] = True
             elif attr in 'comment':
                 # ignoring comment attr as explained in the
-                #   function docstring
+                # function docstring
                 continue
             else:
                 cookie_dict[attr] = value
