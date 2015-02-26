bc7c08683123f633568ba814d176248596603ae5
Rodrigo <rodrigosiqueiramelo@gmail.com>
2015-02-26 10:53:30 -0300

    Added log to views.py

--- revproxy/views.py
+++ revproxy/views.py
@@ -15,7 +15,7 @@ from django.views.generic import View
 from django.utils.decorators import classonlymethod
 
 from .response import get_django_response
-from .utils import normalize_headers, encode_items, setup_logger
+from .utils import normalize_headers, encode_items
 from .transformer import DiazoTransformer
 
 
@@ -35,6 +35,7 @@ class ProxyView(View):
             self._rewrite.append((from_re, to_pattern))
         self.http = urllib3.PoolManager()
         self.log = logging.getLogger('revproxy.view')
+        self.log.info("ProxyView created")
 
     @property
     def upstream(self):
@@ -44,41 +45,46 @@ class ProxyView(View):
     def diazo_rules(self):
         child_class_file = sys.modules[self.__module__].__file__
         app_path = os.path.abspath(os.path.dirname(child_class_file))
-        self.log.debug("diazo_rules: {}".format(app_path))
+        diazo_path = os.path.join(app_path, 'diazo.xml')
 
-        return os.path.join(app_path, 'diazo.xml')
+        self.log.debug("diazo_rules: {}".format(diazo_path))
+        return diazo_path
 
     @classonlymethod
     def as_view(cls, **initkwargs):
         view = super(ProxyView, cls).as_view(**initkwargs)
         view.csrf_exempt = True
-        self.log.info("Exempted view the protection ensured by the middleware.")
         return view
 
     def dispatch(self, request, path):
 
         # Rewrite implementation
         full_path = request.get_full_path()
+        self.log.debug("Dispatch full path: {}".format(full_path))
         for from_re, to_pattern in self._rewrite:
             if from_re.match(full_path):
                 redirect_to = from_re.sub(to_pattern, full_path)
-                self.debug_log.debug("Redirect to: {}".format(redirect_to))
+                self.log.debug("Redirect to: {}".format(redirect_to))
                 return redirect(redirect_to)
 
         request_payload = request.body
         request_headers = normalize_headers(request)
+        self.log.debug("Request headers: {}".format(request_headers))
 
         if self.add_remote_user and request.user.is_active:
             request_headers['REMOTE_USER'] = request.user.username
+            self.log.info("REMOTE_USER set")
 
         request_url = urljoin(
             self.upstream,
             quote(path.encode('utf8'))
         )
+        self.log.debug("Request URL: {}".format(request_url))
 
         if request.GET:
             get_data = encode_items(request.GET.lists())
             request_url += '?' + urlencode(get_data)
+            self.log.debug("Request URL: {}".format(request_url)) 
 
         try:
             proxy_response = self.http.urlopen(request.method,
@@ -88,7 +94,7 @@ class ProxyView(View):
                                                body=request_payload,
                                                decode_content=False,
                                                preload_content=False)
-            self.debug_log.debug("Proxy response header: {}".format(
+            self.log.debug("Proxy response header: {}".format(
                             proxy_response.getheaders()))
         except urllib3.exceptions.HTTPError as error:
             self.log.exception(error)
@@ -110,11 +116,16 @@ class ProxyView(View):
             location = location.replace(upstream_host_https, request_host)
             proxy_response.headers['Location'] = location
 
+            self.log.debug("Proxy response LOCATION: {}".format(
+                                        proxy_response.headers['Location']))
+
         content_type = proxy_response.headers.get('Content-Type')
         if not content_type:
             content_type = (mimetypes.guess_type(request.path)[0] or
                             'application/octet-stream')
             proxy_response.headers['Content-Type'] = content_type
+            self.log.debug("Proxy response CONTENT-TYPE: {}").format(
+                                    proxy_response.headers['Content-Type'])
 
         response = get_django_response(proxy_response)
 
@@ -124,4 +135,5 @@ class ProxyView(View):
                                        self.diazo_theme_template,
                                        self.html5)
 
+        self.log.debug("RESPONSE RETURNED: {}".format(response))
         return response
