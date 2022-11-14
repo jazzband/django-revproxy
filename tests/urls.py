11a6ac23bc78eb4213cf76b21832f54dc809c6ca
Andruten <andruten@users.noreply.github.com>
2022-11-15 00:31:21 +0100

    Drop support for python 2.7, 3.5 and 3.7. Add Support for 3.11 and django 4.x

--- tests/urls.py
+++ tests/urls.py
@@ -1,10 +1,7 @@
-from django.conf.urls import url
-try:
-    from django.contrib.auth.views import login
-except ImportError:
-    # Django 2.2 moved this
-    from django.contrib.auth import login
+from django.urls import path
+
+from django.contrib.auth import login
 
 urlpatterns = [
-    url(r'^accounts/login/$', login, name='login'),
+    path('accounts/login/', login, name='login'),
 ]
